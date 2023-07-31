807b34dc17c764b41c8a9db0868844e9c7e76d25
Felipe <felipe.daniel.henriquez@gmail.com>
2023-07-31 19:04:33 -0400

    se agrega la ruta de argumentos por url yu se configura

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-country-page',
@@ -6,6 +8,19 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit{
+  constructor(
+     private activatedRoute: ActivatedRoute ,
+     private countriesService: CountriesService
+     ) {}
 
+  ngOnInit(): void {
+    this.activatedRoute.params
+    .subscribe( ({ id }) => {
+      this.countriesService.searchCountryByAlphaCode( id )
+      .subscribe( country => {
+        console.log({country})
+      })
+    })
+  }
 }
