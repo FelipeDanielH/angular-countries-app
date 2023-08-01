d54d14abc3e57d39a9e3397778ef941edab9fa11
Felipe <felipe.daniel.henriquez@gmail.com>
2023-08-01 07:51:16 -0400

    se refactoriza el ngOnInit y el metodo del servicio searchByAlphaCode

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -8,19 +9,24 @@ import { CountriesService } from '../../services/countries.service';
   styles: [
   ]
 })
-export class CountryPageComponent implements OnInit{
+export class CountryPageComponent implements OnInit {
   constructor(
-     private activatedRoute: ActivatedRoute ,
-     private countriesService: CountriesService
-     ) {}
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private countriesService: CountriesService
+  ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params
-    .subscribe( ({ id }) => {
-      this.countriesService.searchCountryByAlphaCode( id )
-      .subscribe( country => {
-        console.log({country})
-      })
-    })
+      .pipe(
+        switchMap(({ id }) => this.countriesService.searchCountryByAlphaCode(id)),
+      )
+      .subscribe(country => {
+        if (!country) {
+          return this.router.navigateByUrl('');
+        }
+        console.log('TENEMOS UN PAIS')
+        return
+      });
   }
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of, tap } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
@@ -10,11 +10,12 @@ export class CountriesService {
 
   constructor(private http: HttpClient) { }
 
-  searchCountryByAlphaCode( code: string): Observable<Country[]>{
+  searchCountryByAlphaCode( code: string): Observable<Country | null>{
     const url = `${this.apiUrl}/alpha/${code}`
     return this.http.get<Country[]>(url)
     .pipe(
-      catchError( () => of([]) )
+      map( countries => countries.length > 0 ? countries[0]: null),
+      catchError( () => of(null) )
     )
   }
 
