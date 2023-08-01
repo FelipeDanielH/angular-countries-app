15f6550ba579ab645e7fce318a81f42922eb9707
Felipe <felipe.daniel.henriquez@gmail.com>
2023-08-01 19:04:10 -0400

    se añade debounce sobre la pagina

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -11,12 +11,16 @@ import { CountriesService } from '../../services/countries.service';
 export class ByCountryPageComponent {
 
   public countries: Country[] = []
+  public isLoading: boolean = false
+
   constructor(private countriesService: CountriesService) { }
 
   searchByCountry(term: string): void {
+    this.isLoading = true
     this.countriesService.searchCountry(term)
       .subscribe(countries => {
         this.countries = countries
+        this.isLoading = false
       })
   }
 }
