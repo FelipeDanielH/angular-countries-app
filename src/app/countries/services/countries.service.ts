efdde5cb07d967449158edc1e71cabce15321588
Felipe <felipe.daniel.henriquez@gmail.com>
2023-08-01 16:27:26 -0400

    refactorizado

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, delay, map, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
@@ -10,8 +10,16 @@ export class CountriesService {
 
   constructor(private http: HttpClient) { }
 
+  private getCountriesRequest( url: string ): Observable<Country[]>{
+    return this.http.get<Country[]>(url)
+    .pipe(
+      catchError( () => of([])),
+      delay(2000),
+    )
+  }
+
   searchCountryByAlphaCode( code: string): Observable<Country | null>{
-    const url = `${this.apiUrl}/alpha/${code}`
+    const url: string = `${this.apiUrl}/alpha/${code}`
     return this.http.get<Country[]>(url)
     .pipe(
       map( countries => countries.length > 0 ? countries[0]: null),
@@ -21,32 +29,17 @@ export class CountriesService {
 
   searchCapital(term: string): Observable<Country[]> {
   const url: string = `${this.apiUrl}/capital/${term}`
-  return this.http.get<Country[]>(url)
-  .pipe(
-      catchError( () => of([]))
-    )
+  return this.getCountriesRequest(url)
   }
 
   searchCountry(term: string): Observable<Country[]> {
   const url: string = `${this.apiUrl}/name/${term}`
-  return this.http.get<Country[]>(url)
-  .pipe(
-      catchError( (error) => {
-        console.log(error)
-        return of([])
-      })
-    )
+  return this.getCountriesRequest(url)
   }
 
   searchRegion(term: string): Observable<Country[]> {
   const url: string = `${this.apiUrl}/region/${term}`
-  return this.http.get<Country[]>(url)
-  .pipe(
-      catchError( (error) => {
-        console.log(error)
-        return of([])
-      })
-    )
+  return this.getCountriesRequest(url)
   }
 
 
