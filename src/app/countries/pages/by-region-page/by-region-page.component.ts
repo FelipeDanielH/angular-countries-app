624daa31836624ffe838a7f609fa42c7f688b599
Felipe <felipe.daniel.henriquez@gmail.com>
2023-08-01 19:05:11 -0400

    se añaden botones en vez de searchBox

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = "Africa"|"Americas"|"Asia"|"Europe"|"Oceania"
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,11 +13,16 @@ import { CountriesService } from '../../services/countries.service';
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public regions: Region[] = ["Africa", "Americas","Asia","Europe","Oceania"]
+  public selectedRegion?: Region
 
   constructor( private countriesService:CountriesService ){ }
 
-  searchByRegion(term:string):void {
-    this.countriesService.searchRegion(term)
+  searchByRegion(region:Region):void {
+
+    this.selectedRegion = region
+
+    this.countriesService.searchRegion(region)
     .subscribe( (countries) =>
       this.countries = countries
     )
