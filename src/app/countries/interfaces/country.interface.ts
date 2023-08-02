eda27eb9881dadde611d9670811c32080e5fbc21
Felipe <felipe.daniel.henriquez@gmail.com>
2023-08-02 08:24:28 -0400

    se cambia el nombre del archivo

---
+++ src/app/countries/interfaces/country.interface.ts
@@ -0,0 +1,121 @@
+export interface Country {
+  name:         Name;
+  tld:          string[];
+  cca2:         string;
+  ccn3:         string;
+  cca3:         string;
+  cioc:         string;
+  independent:  boolean;
+  status:       string;
+  unMember:     boolean;
+  currencies:   Currencies;
+  idd:          Idd;
+  capital:      string[];
+  altSpellings: string[];
+  region:       string;
+  subregion:    string;
+  languages:    Languages;
+  translations: { [key: string]: Translation };
+  latlng:       number[];
+  landlocked:   boolean;
+  borders?:     string[];
+  area:         number;
+  demonyms:     Demonyms;
+  flag:         string;
+  maps:         Maps;
+  population:   number;
+  fifa:         string;
+  car:          Car;
+  timezones:    string[];
+  continents:   string[];
+  flags:        Flags;
+  coatOfArms:   CoatOfArms;
+  startOfWeek:  string;
+  capitalInfo:  CapitalInfo;
+  postalCode?:  PostalCode;
+  gini?:        { [key: string]: number };
+}
+
+export interface CapitalInfo {
+  latlng: number[];
+}
+
+export interface Car {
+  signs: string[];
+  side:  string;
+}
+
+export interface CoatOfArms {
+  png?: string;
+  svg?: string;
+}
+
+export interface Currencies {
+  EUR?: Clp;
+  USD?: Clp;
+  DOP?: Clp;
+  CLP?: Clp;
+  YER?: Clp;
+  CRC?: Clp;
+}
+
+export interface Clp {
+  name:   string;
+  symbol: string;
+}
+
+export interface Demonyms {
+  eng: Eng;
+  fra: Eng;
+}
+
+export interface Eng {
+  f: string;
+  m: string;
+}
+
+export interface Flags {
+  png:  string;
+  svg:  string;
+  alt?: string;
+}
+
+export interface Idd {
+  root:     string;
+  suffixes: string[];
+}
+
+export interface Languages {
+  ita?: string;
+  spa?: string;
+  eng?: string;
+  ara?: string;
+}
+
+export interface Maps {
+  googleMaps:     string;
+  openStreetMaps: string;
+}
+
+export interface Name {
+  common:     string;
+  official:   string;
+  nativeName: NativeName;
+}
+
+export interface NativeName {
+  ita?: Translation;
+  spa?: Translation;
+  eng?: Translation;
+  ara?: Translation;
+}
+
+export interface Translation {
+  official: string;
+  common:   string;
+}
+
+export interface PostalCode {
+  format: string;
+  regex:  string;
+}
